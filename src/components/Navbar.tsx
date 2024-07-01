50bab3b1e69327010bc7c0ee68bc443498e7437c
Andry Ariadi <andryariadi23@gmail.com>
2024-07-01 13:37:32 +0700

    add search component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,20 +8,21 @@ import { PiUserCircleThin } from "react-icons/pi";
 import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import { IoIosNotificationsOutline } from "react-icons/io";
+import Search from "./Search";
 
 const Navbar = () => {
   return (
     <>
       <div className="bg-amber-500 h-24 flex items-center justify-between">
         {/* Left */}
-        <div className="bg-rose-500 w-[20%] md:hidden lg:block">
+        <div className="bg-rose-500 w-[20%] h-full md:hidden lg:flex items-center">
           <Link href="/">
             <Image src="/logo.svg" alt="Gramify" width={100} height={100} />
           </Link>
         </div>
 
         {/* Center */}
-        <div className="bg-sky-500 w-[50%] hidden md:flex gap-6 text-sm">
+        <div className="bg-sky-500 w-[50%] h-full hidden md:flex gap-6 text-sm">
           <Link href="/" className="flex items-center gap-2">
             <GoHome size={22} />
             <span>Home</span>
@@ -36,21 +37,24 @@ const Navbar = () => {
           </Link>
         </div>
 
+        <div className="bg-teal-500">
+          <Search />
+        </div>
+
         {/* Right */}
-        <div className="bg-violet-500 w-[30%] flex items-center justify-end">
+        <div className="bg-violet-500 w-[30%] h-full flex items-center justify-end">
           <ClerkLoading>
-            <div
-              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-              role="status"
-            />
+            <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
           </ClerkLoading>
 
           <ClerkLoaded>
             <SignedIn>
-              <HiOutlineUserGroup size={22} />
-              <IoChatbubbleEllipsesOutline size={22} />
-              <IoIosNotificationsOutline size={22} />
-              <UserButton />
+              <div className="bg-rose-500 flex items-center gap-6">
+                <HiOutlineUserGroup size={25} />
+                <IoChatbubbleEllipsesOutline size={25} />
+                <IoIosNotificationsOutline size={25} />
+                <UserButton />
+              </div>
             </SignedIn>
 
             <SignedOut>
