bb5c1f5623a0452870ba8625801a8c9349bef528
Andry Ariadi <andryariadi23@gmail.com>
2024-07-02 06:48:13 +0700

    styling stories comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import AddPost from "@/components/AddPost";
+import Feed from "@/components/Feed";
 import LeftMenu from "@/components/LeftMenu";
 import RightMenu from "@/components/RightMenu";
 import Stories from "@/components/Stories";
@@ -5,7 +7,7 @@ import Stories from "@/components/Stories";
 const Homepage = () => {
   return (
     <>
-      <div className="bg-amber-500 flex gap-5">
+      <div className="bg-amber-500 flex gap-5 pt-6">
         {/* Left */}
         <div className="bg-sky-500 hidden xl:block w-[20%]">
           <LeftMenu />
@@ -13,7 +15,11 @@ const Homepage = () => {
 
         {/* Center */}
         <div className="bg-violet-500 w-full lg:w-[70%] xl:w-[50%]">
-          <Stories />
+          <div className="bg-rose-500 flex flex-col gap-6">
+            <Stories />
+            <AddPost />
+            <Feed />
+          </div>
         </div>
 
         {/* Right */}
--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 const Stories = () => {
-  return <div>Stories</div>;
+  return (
+    <>
+      <div className="stories bg-white p-4 rounded-lg shadow-md overflow-x-scroll text-xs">
+        <div className="bg-violet-700 flex gap-8 w-max">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+            <div key={item} className="flex flex-col items-center gap-2 cursor-pointer">
+              <Image src="https://images.pexels.com/photos/3913025/pexels-photo-3913025.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Stories" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
+              <span>Andry</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Stories;
