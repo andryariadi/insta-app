310fdcb5f9224452d1dda79992cf9d28920c4d8d
Andry Ariadi <andryariadi23@gmail.com>
2024-07-13 16:39:09 +0700

    update styling search comp in navbar comp

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,36 +1,13 @@
 const Search = () => {
   return (
     <>
-      <div className="relative flex" data-twe-input-wrapper-init data-twe-input-group-ref>
-        <input
-          type="search"
-          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-          placeholder="Search"
-          aria-label="Search"
-          id="exampleFormControlInput"
-          aria-describedby="basic-addon1"
-        />
-
-        <label
-          htmlFor="exampleFormControlInput"
-          className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-        >
-          Search
-        </label>
-
-        <button
-          className="relative z-[2] -ms-0.5 flex items-center rounded-e bg-primary px-5  text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-          type="button"
-          id="button-addon1"
-          data-twe-ripple-init
-          data-twe-ripple-color="light"
-        >
-          <span className="[&>svg]:h-5 [&>svg]:w-5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-          </span>
-        </button>
+      <div className="group p-5 overflow-hidden w-[20px] h-[20px] hover:w-[270px] bg-logo shadow-[2px_2px_20px_rgba(0,0,0,0.08)] rounded-full flex group items-center justify-center hover:duration-300 duration-300">
+        <div className="flex items-center justify-center fill-white ">
+          <svg xmlns="http://www.w3.org/2000/svg" id="Isolation_Mode" data-name="Isolation Mode" viewBox="0 0 24 24" width="15" height="15">
+            <path d="M18.9,16.776A10.539,10.539,0,1,0,16.776,18.9l5.1,5.1L24,21.88ZM10.5,18A7.5,7.5,0,1,1,18,10.5,7.507,7.507,0,0,1,10.5,18Z"></path>
+          </svg>
+        </div>
+        <input type="text" className="hidden group-hover:flex outline-none text-[20px] bg-transparent w-full text-white text-xs px-4" />
       </div>
     </>
   );
